39312a234189dcf98de936d1eb4caadc79ac3ecb
Md Ahmed Alauddin <ahmedaladdin0102@gmail.com>
2026-09-08 17:30:43 +0000

    feat: Test logic update shop details from active contract

--- airplane_mode/airport_shop_management/doctype/shop_contract/test_shop_contract.py
+++ airplane_mode/airport_shop_management/doctype/shop_contract/test_shop_contract.py
@@ -2,6 +2,8 @@
 # See license.txt
 
 # import frappe
+from airplane_mode.airport_shop_management.doctype.shop_contract import shop_contract
+import frappe
 from frappe.tests import IntegrationTestCase
 
 
@@ -19,4 +21,19 @@ class IntegrationTestShopContract(IntegrationTestCase):
 	Use this class for testing interactions between multiple components.
 	"""
 
-	pass
+	def test_shop_name_update_on_contract_activation(self):
+		shop = frappe.get_doc("Shop", "Shop-003")
+		tenant = frappe.get_doc("Shop Tenant", "TEN-FASIAL-AHMED001")
+		# Create Shop Contract
+		shop_contract = frappe.new_doc("Shop Contract")
+		shop_contract.shop = shop.name
+		shop_contract.tenant = tenant.name
+		shop_contract.contract_start_date = "2024-01-01"
+		shop_contract.contract_end_date = "2024-12-31"
+		shop_contract.rent_amount = 1000
+		shop_contract.security_deposit = 500
+		shop_contract.status = "Active"
+		shop_contract.shop_display_name = "New Shop Name"
+		shop_contract.insert()
+		shop = frappe.get_doc("Shop", shop_contract.shop)
+		self.assertEqual(shop.shop_name, "New Shop Name")

2b642fc9e6eba6645ba053d9339c1161edb52137
Md Ahmed Alauddin <ahmedaladdin0102@gmail.com>
2026-09-10 16:18:58 +0000

    feat: stop the doctype for submiting if the status is pending or unpaid

--- airplane_mode/airport_shop_management/doctype/shop_rent_payment/shop_rent_payment.py
+++ airplane_mode/airport_shop_management/doctype/shop_rent_payment/shop_rent_payment.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, Ahmed Ansari and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -17,15 +17,19 @@ class ShopRentPayment(Document):
 		amended_from: DF.Link | None
 		amount_due: DF.Currency
 		amount_paid: DF.Currency
-		due_date: DF.Date
+		due_date: DF.Int
 		lease_contract: DF.Link
 		naming_series: DF.Literal["RCPT-.YYYY.-.##"]
 		payment_date: DF.Date
 		payment_mode: DF.Literal["Cash", "UPI", "Credit Card", "Cheque"]
 		payment_month: DF.Literal["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
 		shop: DF.Link
-		status: DF.Literal["Pending", "Paid", "Overdue", "Partially Paid"]
+		status: DF.Literal["Pending", "Paid", "Unpaid"]
 		tenant: DF.Link
 	# end: auto-generated types
+
+	def on_submit(self):
+		if self.status in ('Pending','Overdue','Partially Paid'):
+			frappe.throw('You cannot submit the Payment Reciept until the status is Paid')
 
 	_DOCTYPE_NAME = "Shop Rent Payment"

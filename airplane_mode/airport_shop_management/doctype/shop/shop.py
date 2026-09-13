00ba0a8633093e5c8df3c01e718b6987f251272f
Md Ahmed Alauddin <ahmedaladdin0102@gmail.com>
2026-09-13 11:28:48 +0000

    feat:Auto-publish shops based on status

--- airplane_mode/airport_shop_management/doctype/shop/shop.py
+++ airplane_mode/airport_shop_management/doctype/shop/shop.py
@@ -2,10 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Shop(Document):
+class Shop(WebsiteGenerator):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
@@ -15,14 +15,28 @@ class Shop(Document):
 		from frappe.types import DF
 
 		airport: DF.Link
-		area: DF.Data | None
+		area: DF.Data
 		current_contract: DF.Link | None
 		current_tenant: DF.Link | None
+		is_published: DF.Check
 		location: DF.Data | None
+		route: DF.Data | None
 		shop_category: DF.Link | None
 		shop_name: DF.Data | None
 		shop_number: DF.Data
-		status: DF.Literal["Available", "Lease", "Under Construction"]
+		shop_type: DF.Link | None
+		status: DF.Literal["Available", "Leased", "Under Construction"]
 	# end: auto-generated types
+
+	#Shop Number Naming convention
+def before_save(self):
+	if self.status=='Available':
+		self.is_published=1
+	else:
+		self.is_published=0
+
+
+		
+	
 
 	_DOCTYPE_NAME = "Shop"
